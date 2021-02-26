7bee88937060439e59ce8e8b9d317c63bd7094cd
Christophe Hautenne <christophe.hautenne@zol.fr>
2021-02-26 07:48:05 +0100

    Improve debug

--- src/Service/SenderService.php
+++ src/Service/SenderService.php
@@ -7,11 +7,16 @@ class SenderService
     public static function track()
     {
         // Get the last 3 items of the backtrace (the current call to track(), the called and the caller)
-        $backtrace = debug_backtrace(false, 4);
+        $backtrace = debug_backtrace(false, 3);
 
         // Shift the call to this function track()
         array_shift($backtrace);
 
-        dump($backtrace);
+        $call = sprintf(
+            '%s%s%s() ==> %s%s%s()',
+            $backtrace[1]['class'], $backtrace[1]['type'], $backtrace[1]['function'],
+            $backtrace[0]['class'], $backtrace[0]['type'], $backtrace[0]['function']
+        );
+        dump($call);
     }
 }
